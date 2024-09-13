7f4dcc5cba2664c905608e33bb203e7abe5584ae
Cade Larrabee <larrabeecade@gmail.com>
2024-09-13 14:51:17 -0400

    Adding outlet for details

--- ContactApp/src/main.jsx
+++ ContactApp/src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Root from "./routes/root";
+import Contact from "./routes/contact";
+import ErrorPage from "./error-page";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
